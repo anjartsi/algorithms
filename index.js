3c5ff01c0f8b447ecbda42957d693bd9a7b5c8e6
Armen Ourfalian <aourfalian@gmail.com>
2017-09-27 14:41:02 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+let express = require('express');
+let app = express();
+// Use Pug as the template engine
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+// Use Stylus to make css less annoying
+let stylus = require('stylus');
+app.use(stylus.middleware('./public/styles'))
+
+// Prepare server for bootstrap/jquery
+app.use('/', express.static('./www')); // redirect root
+app.use('/js', express.static('./node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static('./node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/css', express.static('./node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+
+
+// Use body-parser to read form data
+// This must come before using the router
+let bodyParser = require('body-parser');
+app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// Use multer to handling multi-part forms (used for uploading files)
+let multer = require('multer');
+let upload = multer({dest: 'uploads/'});
+
+
+let nav = [
+	  {link: '/', text: 'Home'}
+];
+
+
+// Use the routes file for all routes to minimize the size of this file
+// let router = require('./router.js');
+// app.use('/', router);
+app.get('/', function(req, res) {
+	res.render("home.pug" , {
+		title: "Algorithm Visualization",
+		nav: nav,
+		projects: {}
+	});
+})
+// Use the public folder to serve all static files
+app.use(express.static('public'));
+app.use(express.static('uploads'));
+
+app.listen(5000);
